aae5fb2a2ac6807b99fb9eed803d854c55537646
Deibis <adayam85@gmail.com>
2024-10-08 14:01:44 -0500

    Segunda actualización

---
+++ config/conexion.js
@@ -0,0 +1,19 @@
+var mysql= require ("mysql");
+var con= mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password:'',
+    database:'solution'
+});
+con.connect(
+    (err)=>{
+
+    if(!err){
+              console.log('Conexion establecida');
+
+    } else{
+
+              console.log('Error de conexion');  
+    }
+    });
+    module.exports=con;
---
+++ controllers/usuariosController.js
@@ -0,0 +1,16 @@
+var con= require('../config/conexion');
+
+
+module.exports={
+
+index:function(req,res){
+
+   con.query("SELECT * FROM usuarios", function (err,datos){
+        console.log(datos);
+    });
+
+
+
+    
+    res.render ('usuarios/index',{title: 'Aplicacion'});
+}}
